8c1d1f085bd0db6cdadc03cc4b4ef6d6ef804956
MateusDaroz <matlicoop@gmail.com>
2025-02-21 00:20:46 -0300

    add: login page inputs

---
+++ lib/login.dart
@@ -0,0 +1,112 @@
+import 'package:flutter/material.dart';
+
+class Login extends StatelessWidget {
+  const Login({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    bool isChecked = false;
+
+    return Container(
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          begin: Alignment(0, 0),
+          end: Alignment(1, 1),
+          transform: GradientRotation(45),
+          colors: [
+            Color.fromRGBO(25, 24, 29, 1),
+            Color.fromRGBO(12, 58, 96, 1),
+          ],
+        ),
+      ),
+      child: Scaffold(
+        backgroundColor: Colors.transparent,
+        body: Center(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+                Container(
+                  padding: EdgeInsets.only(top: 50, bottom: 10),
+                  child: Image.asset('img/jurailogo.png', scale: 1.0,),
+                ),
+
+                Image.asset('img/jurai-name_resized.png', scale: 2.0,),
+
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  spacing: 20,
+                  children: [
+                    Container(
+                      padding: EdgeInsets.fromLTRB(0, 35, 0, 20),
+                      child: Text("Bem-Vindo(a) de volta", style: TextStyle(fontSize: 30, color: Colors.white), textAlign: TextAlign.center,),
+                    ),
+                  ],
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  spacing: 20,
+                  children: [
+                    Container(
+                      padding: EdgeInsets.only(bottom: 15),
+                      child: Text("Insira suas credenciais para\nefetuar o login", style: TextStyle(fontSize: 20, color: Colors.white), textAlign: TextAlign.center,),
+                    ),
+                  ],
+                ),
+                Container(
+                  margin: EdgeInsets.symmetric(horizontal: 50, vertical: 10),
+                  padding: EdgeInsets.symmetric(vertical: 8, horizontal: 16),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10),
+                    gradient: LinearGradient(
+                      begin: Alignment(0, 0),
+                      end: Alignment(1, 1),
+                      colors: [
+                        Color(0x3E387FB9),
+                        Color(0x3E2B2932),
+                      ],
+                    ),
+                  ),
+                  child: TextField(
+                    decoration: InputDecoration(fillColor: Colors.transparent, border: InputBorder.none, hintText: "E-mail", hintStyle: TextStyle(color: Colors.grey)),
+                    style: TextStyle(color: Colors.white),
+                  ),
+                ),
+                Container(
+                  margin: EdgeInsets.symmetric(horizontal: 50, vertical: 10),
+                  padding: EdgeInsets.symmetric(vertical: 8, horizontal: 16),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10),
+                    gradient: LinearGradient(
+                      begin: Alignment(0, 0),
+                      end: Alignment(1, 1),
+                      colors: [
+                        Color(0x3E387FB9),
+                        Color(0x3E2B2932),
+                      ],
+                    ),
+                  ),
+                  child: TextField(
+                    decoration: InputDecoration(fillColor: Colors.transparent, border: InputBorder.none, hintText: "Senha", hintStyle: TextStyle(color: Colors.grey)),
+                    style: TextStyle(color: Colors.white),
+                    obscureText: true,
+                  ),
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    
+                    CheckboxListTile(title: Text("Manter-me conectado"), value: isChecked, onChanged: (value){
+                      setState(() {
+                        isChecked = value!;
+                      });
+                    })
+                  ],
+
+                )
+            ]
+          )
+        )
+      )
+    );
+  }
+}
