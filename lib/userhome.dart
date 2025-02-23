5eed2f8eb8841d4f0f732c9204af2e161fd7a89c
MateusDaroz <matlicoop@gmail.com>
2025-02-23 10:28:49 -0300

    add: userhome page

---
+++ lib/userhome.dart
@@ -0,0 +1,93 @@
+import 'package:flutter/material.dart';
+import 'package:jurai/profile.dart';
+
+class UserHome extends StatelessWidget {
+  const UserHome({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          begin: Alignment(0, 0),
+          end: Alignment(1, 1),
+          transform: GradientRotation(45),
+          colors: [
+            Color.fromRGBO(25, 24, 29, 1),
+            Color.fromRGBO(12, 58, 96, 1),
+          ],
+        ),
+      ),
+      child: Scaffold(
+        backgroundColor: Colors.transparent,
+        body: Center(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  Container(
+                    padding: EdgeInsets.only(top: 50, bottom: 10),
+                    child: Image.asset('img/jurai-name.png', scale: 4.0),
+                  ),
+                  Container(
+                    padding: EdgeInsets.only(top: 50, bottom: 10),
+                    child: ElevatedButton(
+                      onPressed: (){Navigator.push(context, MaterialPageRoute(builder: (context) => const Profile()));},
+                      style: ElevatedButton.styleFrom(
+                        shape: CircleBorder(),
+                        padding: EdgeInsets.all(20),
+                        backgroundColor: Colors.amber,
+                      ),
+                      child: Image.asset("img/profile.png"),
+                    ),
+                  ),
+                ],
+              ),
+              Row(
+                
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  Container(
+                    padding: EdgeInsets.fromLTRB(50, 50, 50, 20),
+                    child: Text(
+                      "Olá, {advogado.username}",
+                      style: TextStyle(color: Colors.white, fontSize: 35),
+                    ),
+                  ),
+                ],
+              ),
+              Row(
+                
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  Container(
+                    padding: EdgeInsets.symmetric(horizontal: 50),
+                    child: Text(
+                      "O que deseja fazer hoje?",
+                      style: TextStyle(color: Colors.white, fontSize: 20),
+                    ),
+                  ),
+                ],
+              ),
+              Row(
+                
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  Container(
+                    padding: EdgeInsets.symmetric(horizontal: 50, vertical: 35),
+                    child: Text(
+                      "Acessos Recentes",
+                      style: TextStyle(color: Colors.white, fontSize: 35),
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
