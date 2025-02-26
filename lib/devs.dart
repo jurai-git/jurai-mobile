c7b1d8067137772d33cc232e9914fd480ca529ca
MateusDaroz <matlicoop@gmail.com>
2025-02-25 23:27:26 -0300

    add: carrousel of devs

--- lib/devs.dart
+++ lib/devs.dart
@@ -1,6 +1,7 @@
 import 'dart:io';
-
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
+
 
 class Devs extends StatelessWidget {
   const Devs({super.key});
@@ -11,7 +12,8 @@ class Devs extends StatelessWidget {
       color: Color.fromRGBO(25, 24, 29, 1),
       child: Scaffold(
         backgroundColor: Colors.transparent,
-        body: Center(
+        body: SingleChildScrollView( 
+        child: Center(
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.center,
             children: [
@@ -64,27 +66,53 @@ class Devs extends StatelessWidget {
                   ),
                 ],
               ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Container(
-                    child: CircleAvatar(backgroundImage: NetworkImage("https://www.petz.com.br/blog/wp-content/uploads/2017/07/passarinhos-domesticos-pet.jpg")),        
-                  ),
-                  Container(
-                    child: CircleAvatar(backgroundImage: NetworkImage("https://www.petz.com.br/blog/wp-content/uploads/2017/07/passarinhos-domesticos-pet.jpg"))
-                  ),
-                  Container(
-                    child: CircleAvatar(backgroundImage: NetworkImage("https://www.petz.com.br/blog/wp-content/uploads/2017/07/passarinhos-domesticos-pet.jpg"))
-                  ),
-                  Container(
-                    child: CircleAvatar(backgroundImage: NetworkImage("https://www.petz.com.br/blog/wp-content/uploads/2017/07/passarinhos-domesticos-pet.jpg"))
-                  ),
-                ],
+              CarouselWithSnap(),
+              Container(
+                margin: EdgeInsets.only(top: 20),
               )
             ]
           )
         )
       )
+      )
     );
+  }
+}
+
+class CarouselWithSnap extends StatelessWidget {
+  final List<Column> imgList = [
+    Column(crossAxisAlignment: CrossAxisAlignment.center, children: [Image.network("https://avatars.githubusercontent.com/u/132313694?v=4", fit: BoxFit.cover, width: 300,), Text("Wendel Frota", style: TextStyle(color: Colors.white, fontSize: 30),), Text("17 Anos", style: TextStyle(color: Colors.white, fontSize: 20),)],),
+    Column(crossAxisAlignment: CrossAxisAlignment.center, children: [Image.network("https://avatars.githubusercontent.com/u/143561347?v=4", fit: BoxFit.cover, width: 300,), Text("Mateus Daroz", style: TextStyle(color: Colors.white, fontSize: 30),), Text("18 Anos", style: TextStyle(color: Colors.white, fontSize: 20),)],),
+    Column(crossAxisAlignment: CrossAxisAlignment.center, children: [Image.network("https://avatars.githubusercontent.com/u/132313694?v=4", fit: BoxFit.cover, width: 300,), Text("Wendel Frota", style: TextStyle(color: Colors.white, fontSize: 30),), Text("17 Anos", style: TextStyle(color: Colors.white, fontSize: 20),)],),
+    Column(crossAxisAlignment: CrossAxisAlignment.center, children: [Image.network("https://avatars.githubusercontent.com/u/143561347?v=4", fit: BoxFit.cover, width: 300,), Text("Mateus Daroz", style: TextStyle(color: Colors.white, fontSize: 30),), Text("18 Anos", style: TextStyle(color: Colors.white, fontSize: 20),)],),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return CarouselSlider(
+          options: CarouselOptions(
+            height: 430.0,
+            enlargeCenterPage: true,
+            enableInfiniteScroll: true,
+            autoPlay: true,
+            autoPlayInterval: Duration(seconds: 3),
+            autoPlayAnimationDuration: Duration(milliseconds: 800),
+            autoPlayCurve: Curves.fastOutSlowIn,
+            scrollDirection: Axis.horizontal,
+            viewportFraction: 0.7,
+            enlargeFactor: 0.2,
+            aspectRatio: 1/1,
+            initialPage: 0,
+            pageSnapping: true,
+          ),
+          items: imgList.map((item) => Container(
+            margin: EdgeInsets.symmetric(horizontal: 20),
+            padding: EdgeInsets.only(top: 28),            
+            color: Color(0xFF2B2932),
+            child: Center(
+              child: item
+            ),
+          )).toList(),
+        );
   }
 }
