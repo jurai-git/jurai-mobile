a2a47032bc6ec3ba003246c69387f57e8f855a7d
MateusDaroz <matlicoop@gmail.com>
2025-02-28 09:48:05 -0300

    refactor: analysis structure and add textfield and button

--- lib/analysis.dart
+++ lib/analysis.dart
@@ -1,9 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:jurai/profile.dart';
 
-class Analysis extends StatelessWidget {
+int buttonIndex = 0;
+
+class Analysis extends StatefulWidget {
   const Analysis({super.key});
 
+  @override
+  _AnalysisState createState() => _AnalysisState();
+}
+
+class _AnalysisState extends State<Analysis> {
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -25,7 +32,14 @@ class Analysis extends StatelessWidget {
                   Container(
                     margin: EdgeInsets.only(right: 50, top: 50),
                     child: ElevatedButton(
-                      onPressed: (){Navigator.push(context, MaterialPageRoute(builder: (context) => const Profile()));},
+                      onPressed: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => const Profile(),
+                          ),
+                        );
+                      },
                       style: ElevatedButton.styleFrom(
                         shape: CircleBorder(),
                         padding: EdgeInsets.all(20),
@@ -45,59 +59,142 @@ class Analysis extends StatelessWidget {
                       "Consulta Rápida",
                       style: TextStyle(color: Colors.white, fontSize: 35),
                     ),
-
                   ),
                 ],
               ),
               Container(
                 padding: EdgeInsets.symmetric(horizontal: 60),
                 width: MediaQuery.of(context).size.width,
-              child:SingleChoice(),
+                child: SingleChoice(
+                  onSelectionChanged: (value) {
+                    setState(() {
+                      buttonIndex = value - 1;
+                    });
+                  },
+                ),
               ),
-            ]
-          )
-        )
-      )
+              if (buttonIndex == 0) RenderPdf(),
+              if (buttonIndex == 1) RenderEment(),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
 
-enum Options {pdf, ementa}
+enum Options { pdf, ementa }
 
-class SingleChoice extends StatefulWidget {
-  const SingleChoice({super.key});
-
-  @override
-  State<SingleChoice> createState() => _SingleChoiceState();
-}
-
-class _SingleChoiceState extends State<SingleChoice> {
-  Options options = Options.pdf;
+class SingleChoice extends StatelessWidget {
+  final Function(int) onSelectionChanged;
+  const SingleChoice({super.key, required this.onSelectionChanged});
 
   @override
   Widget build(BuildContext context) {
     return SegmentedButton<Options>(
       showSelectedIcon: false,
-      style: SegmentedButton.styleFrom(backgroundColor: Color.fromRGBO(120, 120, 128, 0.120), foregroundColor: Color(0x77808080),selectedForegroundColor: Colors.black, side: BorderSide(color: Colors.transparent, width: 0), textStyle: TextStyle(fontWeight: FontWeight.w400)),
+      style: SegmentedButton.styleFrom(
+        backgroundColor: Color.fromRGBO(120, 120, 128, 0.120),
+        foregroundColor: Color(0x77808080),
+        selectedForegroundColor: Colors.black,
+        side: BorderSide(color: Colors.transparent, width: 0),
+        textStyle: TextStyle(fontWeight: FontWeight.w400),
+      ),
       segments: const <ButtonSegment<Options>>[
-        ButtonSegment<Options>(
-          value: Options.pdf,
-          label: Text('PDF'),
-        ),
-        ButtonSegment<Options>(
-          value: Options.ementa,
-          label: Text('Ementa'),
+        ButtonSegment<Options>(value: Options.pdf, label: Text('PDF')),
+        ButtonSegment<Options>(value: Options.ementa, label: Text('Ementa')),
+      ],
+      selected: <Options>{Options.values[buttonIndex]},
+      onSelectionChanged: (Set<Options> newSelection) {
+        Options selectedOption = newSelection.first;
+        int selectedValue = selectedOption == Options.pdf ? 1 : 2;
+        onSelectionChanged(selectedValue);
+      },
+    );
+  }
+}
+
+class RenderPdf extends StatelessWidget {
+  const RenderPdf({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        Container(
+          padding: EdgeInsets.all(20),
+          child: Text(
+            "Selecione o arquivo PDF da sua demanda para realizar a análise  da mesma",
+            style: TextStyle(color: Colors.white, fontSize: 20),
+            textAlign: TextAlign.center,
+          ),
         ),
       ],
-      selected: <Options>{options},
-      onSelectionChanged: (Set<Options> newSelection) {
-        setState(() {
-          // By default there is only a single segment that can be
-          // selected at one time, so its value is always the first
-          // item in the selected set.
-          options = newSelection.first;
-        });
-      },
+    );
+  }
+}
+
+class RenderEment extends StatelessWidget {
+  const RenderEment({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        Container(
+          padding: EdgeInsets.all(20),
+          child: Text(
+            "Digite o texto da sua ementa para fazer a análise da mesma",
+            style: TextStyle(color: Colors.white, fontSize: 20),
+            textAlign: TextAlign.center,
+          ),
+        ),
+        Container(
+          height: MediaQuery.of(context).size.height - 600,
+          margin: EdgeInsets.symmetric(horizontal: 40),
+          width: MediaQuery.of(context).size.width,
+          decoration: BoxDecoration(
+            color: Color(0x772B2932),
+            borderRadius: BorderRadius.circular(50),
+          ),
+          child: TextField(
+            maxLines: null,
+            style: TextStyle(color: Colors.white),
+            decoration: InputDecoration(
+              contentPadding: EdgeInsets.all(20),
+              hintText: "Digite aqui...",
+              border: InputBorder.none,
+            ),
+          ),
+          
+        ),
+        Container(
+          margin: EdgeInsets.only(top: 60),
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(100),
+            gradient: LinearGradient(
+              begin: AlignmentDirectional.topCenter,
+              end: AlignmentDirectional.bottomCenter,
+              colors: [
+                Color(0xFF2B2932),
+                Color(0xFF1E1E1E),
+              ],
+            ),
+          ),
+          child: ElevatedButton(
+            onPressed: () {},
+            style: ButtonStyle(
+              backgroundColor: WidgetStateColor.transparent,
+              fixedSize: WidgetStateProperty.all(Size.fromWidth(250)),
+              padding: WidgetStateProperty.all(EdgeInsets.all(30)),
+            ),
+            child: Text("Analisar", style: TextStyle(color: Colors.white)),
+          ),
+        ),
+      ],
     );
   }
 }
