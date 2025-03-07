bf281fd612d5d27c773340610ec9899ec739e343
MateusDaroz <matlicoop@gmail.com>
2025-03-07 09:31:26 -0300

    deleted: annoying comments

--- lib/login.dart
+++ lib/login.dart
@@ -115,19 +115,11 @@ class LoginForm extends StatefulWidget {
   }
 }
 
-// Define a corresponding State class.
-// This class holds data related to the form.
 class MyCustomFormState extends State<LoginForm> {
-  // Create a global key that uniquely identifies the Form widget
-  // and allows validation of the form.
-  //
-  // Note: This is a `GlobalKey<FormState>`,
-  // not a GlobalKey<MyCustomFormState>.
   final _formKey = GlobalKey<FormState>();
 
   @override
   Widget build(BuildContext context) {
-    // Build a Form widget using the _formKey created above.
     return Form(
       key: _formKey,
       child: Column(
