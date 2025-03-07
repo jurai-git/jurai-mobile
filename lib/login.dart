e34c1a50cc3a0fdd99818ce257bff7bb550cfa50
MateusDaroz <matlicoop@gmail.com>
2025-03-07 09:26:04 -0300

    refactor: textfields on login to textformfield for validation

--- lib/login.dart
+++ lib/login.dart
@@ -73,95 +73,7 @@ class Login extends StatelessWidget {
                   ),
                 ],
               ),
-              Container(
-                margin: EdgeInsets.symmetric(horizontal: 50, vertical: 10),
-                padding: EdgeInsets.symmetric(vertical: 8, horizontal: 16),
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(10),
-                  gradient: LinearGradient(
-                    begin: Alignment(0, 0),
-                    end: Alignment(1, 1),
-                    colors: [Color(0x3E387FB9), Color(0x3E2B2932)],
-                  ),
-                ),
-                child: TextField(
-                  decoration: InputDecoration(
-                    prefixIcon: Image.asset('img/email.png',),
-                    prefixIconConstraints: BoxConstraints(minWidth: 10, minHeight: 10),
-                    fillColor: Colors.transparent,
-                    border: InputBorder.none,
-                    hintText: "E-mail",
-                    hintStyle: TextStyle(color: Colors.grey),
-                  ),
-                  style: TextStyle(color: Colors.white),
-                ),
-              ),
-              Container(
-                margin: EdgeInsets.symmetric(horizontal: 50, vertical: 10),
-                padding: EdgeInsets.symmetric(vertical: 8, horizontal: 16),
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(10),
-                  gradient: LinearGradient(
-                    begin: Alignment(0, 0),
-                    end: Alignment(1, 1),
-                    colors: [Color(0x3E387FB9), Color(0x3E2B2932)],
-                  ),
-                ),
-                child: TextField(
-                  decoration: InputDecoration(
-                    prefixIcon: Image.asset('img/password.png',),
-                    prefixIconConstraints: BoxConstraints(minWidth: 10, minHeight: 10),
-                    fillColor: Colors.transparent,
-                    border: InputBorder.none,
-                    hintText: "Senha",
-                    hintStyle: TextStyle(color: Colors.grey),
-                  ),
-                  style: TextStyle(color: Colors.white),
-                  obscureText: true,
-                ),
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
-                children: [
-                  Container(
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.start,
-                      children: [
-                        Check(),
-                        Text(
-                          "Manter-me conectado",
-                          style: TextStyle(color: Colors.white),
-                        ),
-                      ],
-                    ),
-                  ),
-                  Text(
-                    "Esqueci minha senha",
-                    style: TextStyle(
-                      color: Color(0xFF387FB9),
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                ],
-              ),
-              Spacer(),
-              Container(
-                  margin: EdgeInsets.only(bottom: 40),
-                  decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(100),
-                  gradient: LinearGradient(
-                    begin: AlignmentDirectional.topCenter,
-                    end: AlignmentDirectional.bottomCenter,
-                    colors: [
-                      Color(0xFF387FB9),
-                      Color(0xFF387FB9),
-                      Color(0xFF387FB9),
-                      Color(0x3E1E1E1E),
-                    ],
-                  ),
-                ),
-                  child: ElevatedButton(onPressed: (){Navigator.push(context, MaterialPageRoute(builder: (context) => const Navigation()));}, style: ButtonStyle(backgroundColor: WidgetStateColor.transparent, fixedSize: WidgetStateProperty.all(Size.fromWidth(250)), padding: WidgetStateProperty.all(EdgeInsets.all(30))), child: Text("Criar Conta", style: TextStyle(color: Colors.white),),),
-                ),
+              LoginForm(),
             ],
           ),
         ),
@@ -190,6 +102,175 @@ class CheckState extends State<Check> {
           isChecked = newBool;
         });
       },
+    );
+  }
+}
+
+class LoginForm extends StatefulWidget {
+  const LoginForm({super.key});
+
+  @override
+  MyCustomFormState createState() {
+    return MyCustomFormState();
+  }
+}
+
+// Define a corresponding State class.
+// This class holds data related to the form.
+class MyCustomFormState extends State<LoginForm> {
+  // Create a global key that uniquely identifies the Form widget
+  // and allows validation of the form.
+  //
+  // Note: This is a `GlobalKey<FormState>`,
+  // not a GlobalKey<MyCustomFormState>.
+  final _formKey = GlobalKey<FormState>();
+
+  @override
+  Widget build(BuildContext context) {
+    // Build a Form widget using the _formKey created above.
+    return Form(
+      key: _formKey,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          // Add TextFormFields and ElevatedButton here.
+          Container(
+            margin: EdgeInsets.symmetric(horizontal: 50, vertical: 10),
+            padding: EdgeInsets.symmetric(vertical: 8, horizontal: 16),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(10),
+              gradient: LinearGradient(
+                begin: Alignment(0, 0),
+                end: Alignment(1, 1),
+                colors: [Color(0x3E387FB9), Color(0x3E2B2932)],
+              ),
+            ),
+            child: TextFormField(
+              validator: (value) {
+                if (value == null || value.isEmpty) {
+                  return "O campo e-mail é de preenchimento obrigatório!";
+                } else if (!new RegExp(
+                  "(?:[a-z0-9!#\$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#\$%&'*+/=?^_`{|}~-]+)*|\"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x20\\x21\\x23-\\x5b\\x5d-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)\\])",
+                ).hasMatch(value)) {
+                  return "O campo e-mail está inválido";
+                }
+
+                return null;
+              },
+              decoration: InputDecoration(
+                errorStyle: TextStyle(
+                  color: Color(0xFFD32F2F),
+                  fontWeight: FontWeight.bold,
+                ),
+                prefixIcon: Image.asset('img/email.png'),
+                prefixIconConstraints: BoxConstraints(
+                  minWidth: 10,
+                  minHeight: 10,
+                ),
+                fillColor: Colors.transparent,
+                border: InputBorder.none,
+                hintText: "E-mail",
+                hintStyle: TextStyle(color: Colors.grey),
+              ),
+              style: TextStyle(color: Colors.white),
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.symmetric(horizontal: 50, vertical: 10),
+            padding: EdgeInsets.symmetric(vertical: 8, horizontal: 16),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(10),
+              gradient: LinearGradient(
+                begin: Alignment(0, 0),
+                end: Alignment(1, 1),
+                colors: [Color(0x3E387FB9), Color(0x3E2B2932)],
+              ),
+            ),
+            child: TextFormField(
+              validator: (value){
+                if(value == null || value.isEmpty){
+                  return "O campo e-mail é de preenchimento obrigatório!";
+                }
+                return null;
+              },
+              decoration: InputDecoration(
+                errorStyle: TextStyle(
+                  color: Color(0xFFD32F2F),
+                  fontWeight: FontWeight.bold,
+                ),
+                prefixIcon: Image.asset('img/password.png'),
+                prefixIconConstraints: BoxConstraints(
+                  minWidth: 10,
+                  minHeight: 10,
+                ),
+                fillColor: Colors.transparent,
+                border: InputBorder.none,
+                hintText: "Senha",
+                hintStyle: TextStyle(color: Colors.grey),
+              ),
+              style: TextStyle(color: Colors.white),
+              obscureText: true,
+            ),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              Container(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    Check(),
+                    Text(
+                      "Manter-me conectado",
+                      style: TextStyle(color: Colors.white),
+                    ),
+                  ],
+                ),
+              ),
+              Text(
+                "Esqueci minha senha",
+                style: TextStyle(
+                  color: Color(0xFF387FB9),
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ],
+          ),
+
+          Container(
+            margin: EdgeInsets.only(bottom: 40),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(100),
+              gradient: LinearGradient(
+                begin: AlignmentDirectional.topCenter,
+                end: AlignmentDirectional.bottomCenter,
+                colors: [
+                  Color(0xFF387FB9),
+                  Color(0xFF387FB9),
+                  Color(0xFF387FB9),
+                  Color(0x3E1E1E1E),
+                ],
+              ),
+            ),
+            child: ElevatedButton(
+              onPressed: () {
+                if (_formKey.currentState!.validate()) {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => const Navigation()),
+                  );
+                }
+              },
+              style: ButtonStyle(
+                backgroundColor: WidgetStateColor.transparent,
+                fixedSize: WidgetStateProperty.all(Size.fromWidth(250)),
+                padding: WidgetStateProperty.all(EdgeInsets.all(30)),
+              ),
+              child: Text("Criar Conta", style: TextStyle(color: Colors.white)),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
