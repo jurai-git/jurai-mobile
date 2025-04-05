386cf5fe6d465381d7051b47baa81ea7e2408c83
MateusDaroz <matlicoop@gmail.com>
2025-04-05 11:59:28 -0300

    refactor: register text form fields declaration

--- lib/features/auth/view/pages/register.dart
+++ lib/features/auth/view/pages/register.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:jurai/features/auth/view/pages/homepage.dart';
 import 'package:jurai/features/auth/view/pages/login.dart';
+import 'package:jurai/features/auth/view/widgets/customtextfield.dart';
 import 'package:jurai/features/auth/viewmodel/auth_viewmodel.dart';
 import 'package:jurai/features/home/view/pages/navigation.dart';
 import 'package:jurai/features/home/view/pages/userhome.dart';
@@ -127,223 +128,39 @@ class RegisterState extends ConsumerState<Register>{
                 crossAxisAlignment: CrossAxisAlignment.center,
                 children: [
                   // Add TextFormFields and ElevatedButton here.
-                  Container(
-                    margin: EdgeInsets.symmetric(horizontal: 50, vertical: 10),
-                    padding: EdgeInsets.symmetric(vertical: 8, horizontal: 16),
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(10),
-                      gradient: LinearGradient(
-                        begin: Alignment(0, 0),
-                        end: Alignment(1, 1),
-                        colors: [Color(0x3E387FB9), Color(0x3E2B2932)],
-                      ),
-                    ),
-                    child: TextFormField(
-                      controller: usernameController,
-                      validator: (value) {
-                        if (value == null || value.isEmpty) {
-                          return "O campo nome é de preenchimento obrigatório!";
-                        }
-
-                        return null;
-                      },
-                      decoration: InputDecoration(
-                        errorStyle: TextStyle(
-                          color: Color(0xFFD32F2F),
-                          fontWeight: FontWeight.bold,
-                        ),
-                        prefixIcon: Image.asset('img/profile.png'),
-                        prefixIconConstraints: BoxConstraints(
-                          minWidth: 10,
-                          minHeight: 10,
-                        ),
-                        fillColor: Colors.transparent,
-                        border: InputBorder.none,
-                        hintText: "Nome Completo",
-                        hintStyle: TextStyle(color: Colors.grey),
-                      ),
-                      style: TextStyle(color: Colors.white),
-                    ),
+                  CustomTextField(
+                    textController: usernameController,
+                    type: "username",
                   ),
-                  Container(
-                    margin: EdgeInsets.symmetric(horizontal: 50, vertical: 10),
-                    padding: EdgeInsets.symmetric(vertical: 8, horizontal: 16),
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(10),
-                      gradient: LinearGradient(
-                        begin: Alignment(0, 0),
-                        end: Alignment(1, 1),
-                        colors: [Color(0x3E387FB9), Color(0x3E2B2932)],
-                      ),
-                    ),
-                    child: TextFormField(
-                      controller: oabController,
-                      validator: (value) {
-                        if (value == null || value.isEmpty) {
-                          return "O campo Nº OAB é de preenchimento obrigatório!";
-                        } else if (!RegExp("^[0-9]*\$").hasMatch(value)) {
-                          return "O campo Nº OAB é composto apenas por números!";
-                        }
-                        return null;
-                      },
-                      decoration: InputDecoration(
-                        errorStyle: TextStyle(
-                          color: Color(0xFFD32F2F),
-                          fontWeight: FontWeight.bold,
-                        ),
-                        prefixIcon: Image.asset('img/oab.png'),
-                        prefixIconConstraints: BoxConstraints(
-                          minWidth: 10,
-                          minHeight: 10,
-                        ),
-                        fillColor: Colors.transparent,
-                        border: InputBorder.none,
-                        hintText: "Nº OAB",
-                        hintStyle: TextStyle(color: Colors.grey),
-                      ),
-                      style: TextStyle(color: Colors.white),
-                    ),
+                  CustomTextField(
+                    textController: oabController,
+                    type: "oab",
                   ),
-                  Container(
-                    margin: EdgeInsets.symmetric(horizontal: 50, vertical: 10),
-                    padding: EdgeInsets.symmetric(vertical: 8, horizontal: 16),
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(10),
-                      gradient: LinearGradient(
-                        begin: Alignment(0, 0),
-                        end: Alignment(1, 1),
-                        colors: [Color(0x3E387FB9), Color(0x3E2B2932)],
-                      ),
-                    ),
-                    child: TextFormField(
-                      controller: emailController,
-                      validator: (value) {
-                        if (value == null || value.isEmpty) {
-                          return "O campo e-mail é de preenchimento obrigatório!";
-                        } else if (!RegExp(
-                          "(?:[a-z0-9!#\$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#\$%&'*+/=?^_`{|}~-]+)*|\"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x20\\x21\\x23-\\x5b\\x5d-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)\\])",
-                        ).hasMatch(value)) {
-                          return "O campo e-mail está inválido";
-                        }
-
-                        return null;
-                      },
-                      decoration: InputDecoration(
-                        errorStyle: TextStyle(
-                          color: Color(0xFFD32F2F),
-                          fontWeight: FontWeight.bold,
-                        ),
-                        prefixIcon: Image.asset('img/email.png'),
-                        prefixIconConstraints: BoxConstraints(
-                          minWidth: 10,
-                          minHeight: 10,
-                        ),
-                        fillColor: Colors.transparent,
-                        border: InputBorder.none,
-                        hintText: "E-mail",
-                        hintStyle: TextStyle(color: Colors.grey),
-                      ),
-                      style: TextStyle(color: Colors.white),
-                    ),
+                  CustomTextField(
+                    textController: emailController,
+                    type: "email",
                   ),
-                  Container(
-                    margin: EdgeInsets.symmetric(horizontal: 50, vertical: 10),
-                    padding: EdgeInsets.symmetric(vertical: 8, horizontal: 16),
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(10),
-                      gradient: LinearGradient(
-                        begin: Alignment(0, 0),
-                        end: Alignment(1, 1),
-                        colors: [Color(0x3E387FB9), Color(0x3E2B2932)],
-                      ),
-                    ),
-                    child: TextFormField(
-                      controller: passwordController,
-                      validator: (value) {
-                        if (value == null || value.isEmpty) {
-                          return "O campo senha é de preenchimento obrigatório!";
-                        }
-
-                        return null;
-                      },
-                      textAlignVertical: TextAlignVertical(y: -.5),
-                      decoration: InputDecoration(
-                        errorStyle: TextStyle(
-                          color: Color(0xFFD32F2F),
-                          fontWeight: FontWeight.bold,
-                        ),
-                        prefixIcon: Image.asset('img/password.png'),
-                        prefixIconConstraints: BoxConstraints(
-                          minWidth: 10,
-                          minHeight: 10,
-                        ),
-                        fillColor: Colors.transparent,
-                        border: InputBorder.none,
-                        hintText: "Senha",
-                        hintStyle: TextStyle(color: Colors.grey),
-                        suffixIcon: IconButton(
-                            icon: Image.asset(isHidden ? "img/invisiblePassword.png" : "img/visiblePassword.png"),
-                            color: Colors.white,
-                            onPressed: (){
-                              isHidden = !isHidden;
-                              setState(() {
-                                
-                              });
-                          }, 
-                        ),
-                      ),
-                      style: TextStyle(color: Colors.white),
-                      obscureText: isHidden,
-                    ),
+                  CustomTextField(
+                    textController: passwordController,
+                    type: "password",
+                    obscureText: isHidden,
+                    showSuffix: true,
+                    onSuffixTap: () {
+                      setState(() {
+                        isHidden = !isHidden;
+                      });
+                    },
                   ),
-                  Container(
-                    margin: EdgeInsets.symmetric(horizontal: 50, vertical: 10),
-                    padding: EdgeInsets.symmetric(vertical: 8, horizontal: 16),
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(10),
-                      gradient: LinearGradient(
-                        begin: Alignment(0, 0),
-                        end: Alignment(1, 1),
-                        colors: [Color(0x3E387FB9), Color(0x3E2B2932)],
-                      ),
-                    ),
-                    child: TextFormField(
-                      controller: confirmPasswordController,
-                      validator: (value) {
-                        if (value == null || value.isEmpty) {
-                          return "O campo confirmar senha é de preenchimento obrigatório!";
-                        }
-                        return null;
-                      },
-                      textAlignVertical: TextAlignVertical(y: -.5),
-                      decoration: InputDecoration(
-                        errorStyle: TextStyle(
-                          color: Color(0xFFD32F2F),
-                          fontWeight: FontWeight.bold,
-                        ),
-                        prefixIcon: Image.asset('img/password.png'),
-                        prefixIconConstraints: BoxConstraints(
-                          minWidth: 10,
-                          minHeight: 10,
-                        ),
-                        fillColor: Colors.transparent,
-                        border: InputBorder.none,
-                        hintText: "Confirmar Senha",
-                        hintStyle: TextStyle(color: Colors.grey),
-                        suffixIcon: IconButton(
-                            icon: Image.asset(isConfirmHidden ? "img/invisiblePassword.png" : "img/visiblePassword.png"),
-                            color: Colors.white,
-                            onPressed: (){
-                              isConfirmHidden = !isConfirmHidden;
-                              setState(() {
-                                
-                              });
-                          }, 
-                        ),
-                      ),
-                      style: TextStyle(color: Colors.white),
-                      obscureText: isConfirmHidden,
-                    ),
+                  CustomTextField(
+                    textController: confirmPasswordController,
+                    type: "confirmPassword",
+                    obscureText: isConfirmHidden,
+                    showSuffix: true,
+                    onSuffixTap: () {
+                      setState(() {
+                        isConfirmHidden = !isConfirmHidden;
+                      });
+                    },
                   ),
                 ],
               ),
