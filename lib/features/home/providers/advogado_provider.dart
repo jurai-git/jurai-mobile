9e05dbdfa8408ec1cd947d72753027ac5b695c0c
MateusDaroz <matlicoop@gmail.com>
2025-04-12 11:59:54 -0300

    add: advogadoProvider for save the active account

---
+++ lib/features/home/providers/advogado_provider.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:jurai/features/auth/models/advogado.dart';
+
+class AdvogadoNotifier extends StateNotifier<Advogado?>{
+  AdvogadoNotifier() : super(null);
+
+  void setAdvogado(Advogado advogado){
+    state = advogado;
+  }
+
+  void clear(){
+    state = null;
+  }
+}
+
+final advogadoProvider = StateNotifierProvider<AdvogadoNotifier, Advogado?>((ref) {
+  return AdvogadoNotifier();
+});
