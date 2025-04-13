32a516d30b2e7ee837a0f00b4abe1326f52f349d
MateusDaroz <matlicoop@gmail.com>
2025-04-13 17:38:22 -0300

    add: privacy and security base page

---
+++ lib/features/home/view/pages/privacy_security.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class PrivacySecurity extends StatefulWidget {
+  const PrivacySecurity({super.key});
+
+  @override
+  State<PrivacySecurity> createState() => _PrivacySecurityState();
+}
+
+class _PrivacySecurityState extends State<PrivacySecurity> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
