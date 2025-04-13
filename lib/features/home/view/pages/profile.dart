2db39186cf7bb2235c2c03e1585b11e5638dde1b
MateusDaroz <matlicoop@gmail.com>
2025-04-13 17:39:04 -0300

    refactor: profile options button widget and removed exit account brute code

--- lib/features/home/view/pages/profile.dart
+++ lib/features/home/view/pages/profile.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:jurai/features/auth/view/pages/homepage.dart';
+import 'package:jurai/features/home/view/pages/personal_information.dart';
+import 'package:jurai/features/home/view/pages/privacy_security.dart';
 import 'package:jurai/features/home/view/widgets/profile_options_button.dart';
 
 class Profile extends StatelessWidget {
@@ -42,132 +44,10 @@ class Profile extends StatelessWidget {
                 margin: EdgeInsets.only(bottom: 20),
                 child: Image.asset("img/profile.png", scale: 0.7),
               ),
-              ProfileOptionsButton(text: "Informações Pessoais", preffixIcon: Icons.account_circle),
-              ProfileOptionsButton(text: "Privacidade e Segurança", preffixIcon: Icons.lock),
-              ProfileOptionsButton(text: "Sair", preffixIcon: Icons.logout, quit: true, isLast: true,),
-              Container(
-                decoration: BoxDecoration(
-                  //border: Border.symmetric(horizontal: BorderSide(color: const Color.fromRGBO(255, 255, 255, 0.5), width: 1))
-                ),
-                child: Builder(builder: (context) => 
-                ElevatedButton(
-                  onPressed: () {
-                    Scaffold.of(context).showBottomSheet(
-                      (context) => Stack(
-                        alignment: Alignment.bottomCenter,
-                        children: [
-                          GestureDetector(
-                            onTap: () {
-                              Navigator.pop(context);
-                            },
-                            child: Container(
-                              width: double.infinity,
-                              height: double.infinity,
-                              color: Color.fromRGBO(0, 0, 0, 0.5),
-                            )
-                          ),
-                          Container(
-                            width: double.infinity,
-                            height: 200,
-                            //color: Colors.transparent,
-                            color: Color.fromRGBO(25, 24, 29, 1),
-                            child: Column(
-                              crossAxisAlignment: CrossAxisAlignment.center,
-                              children: [
-                                Row(
-                                  mainAxisAlignment: MainAxisAlignment.center,
-                                  children: [
-                                    Container(
-                                    margin: EdgeInsets.symmetric(vertical: 10),
-                                    child: Text(
-                                      "Você tem certeza que deseja sair da conta?",
-                                      style: TextStyle(color: Colors.white, fontSize: 20),
-                                    ),
-                                    )
-                                  ],
-                                ),
-                                Row(
-                                  mainAxisAlignment: MainAxisAlignment.center,
-                                  children: [
-                                    Container(
-                                    margin: EdgeInsets.symmetric(vertical: 10),
-                                    child: ElevatedButton(
-                                      onPressed: () {
-                                        ScaffoldMessenger.of(
-                                          context,
-                                        ).hideCurrentSnackBar();
-                                        Navigator.push(
-                                          context,
-                                          MaterialPageRoute(
-                                            builder: (context) => Homepage(),
-                                          ),
-                                        );
-                                      },
-                                      style: ElevatedButton.styleFrom(
-                                        backgroundColor: Colors.red,
-                                        fixedSize: Size(
-                                          MediaQuery.of(context).size.width - 50,
-                                          50,
-                                        
-                                        ),
-                                        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(12))
-                                      ),
-                                      child: Text("Sair", style: TextStyle(color: Color.fromRGBO(25, 24, 29, 1),),),
-                                    ),
-                                    ),
-                                  ],
-                                ),
-                                Row(
-                                  mainAxisAlignment: MainAxisAlignment.center,
-                                  children: [
-                                    Container(
-                                    margin: EdgeInsets.symmetric(vertical: 10),
-                                    child: OutlinedButton(
-                                      onPressed: () {
-                                        Navigator.pop(context);
-                                      },
-                                      style: OutlinedButton.styleFrom(
-                                        fixedSize: Size(
-                                          MediaQuery.of(context).size.width - 50,
-                                          50,
-                                        ),
-                                        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(12))
-                                      ),
-                                      child: Text("Ficar", style: TextStyle(color: Colors.white),),
-                                    ),
-                                    ),
-                                  ],
-                                )
-                              ],
-                            ),
-                          ),
-                        ],
-                      ),
-                    backgroundColor: Colors.transparent);
-                  },
-                  style: ElevatedButton.styleFrom(
-                    shadowColor: Colors.transparent,
-                    backgroundColor: Color.fromARGB(255, 25, 24, 29),
-                    padding: EdgeInsets.all(25),
-                    shape: LinearBorder(),
-                  ),
-                  child: Row(
-                    children: [
-                      Icon(Icons.logout, color: Colors.red, size: 30),
-                      Container(
-                        margin: EdgeInsets.symmetric(horizontal: 10),
-                        child: Text(
-                          "Sair da Conta",
-                          style: TextStyle(color: Colors.red, fontSize: 18),
-                        ),
-                      ),
-                      Spacer(),
-                      Icon(Icons.navigate_next, color: Colors.red, size: 30),
-                    ],
-                  ),
-                ),
-                ),
-              ),
+              ProfileOptionsButton(text: "Informações Pessoais", preffixIcon: Icons.account_circle, destiny: PersonalInformation()),
+              ProfileOptionsButton(text: "Privacidade e Segurança", preffixIcon: Icons.lock, destiny: PrivacySecurity()),
+              ProfileOptionsButton(text: "Sair da Conta", preffixIcon: Icons.logout, quit: true, isLast: true, destiny: Homepage()),
+              
             ],
           ),
         ),
