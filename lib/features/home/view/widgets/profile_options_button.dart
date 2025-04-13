7c12d34aba13e5262cbf958ca75ce7157e3f8695
MateusDaroz <matlicoop@gmail.com>
2025-04-13 17:37:31 -0300

    add: destiny attribute to control the navigation between pages

--- lib/features/home/view/widgets/profile_options_button.dart
+++ lib/features/home/view/widgets/profile_options_button.dart
@@ -5,13 +5,15 @@ class ProfileOptionsButton extends StatefulWidget {
   final IconData preffixIcon;
   final bool quit;
   final bool isLast;
+  final Widget destiny;
 
   const ProfileOptionsButton({
     super.key,
     required this.text,
     required this.preffixIcon,
     this.quit = false,
-    this.isLast = false
+    this.isLast = false,
+    required this.destiny
   });
 
   @override
@@ -26,7 +28,102 @@ class _ProfileOptionsButtonState extends State<ProfileOptionsButton> {
         border: Border(bottom: !widget.isLast ? BorderSide(color: const Color.fromRGBO(255, 255, 255, 0.5), width: 1) : BorderSide.none),
       ),
       child: ElevatedButton(
-        onPressed: () {},
+        onPressed: () {
+          !widget.quit ? Navigator.push(context, MaterialPageRoute(builder: (context) => widget.destiny))
+          :
+                    Scaffold.of(context).showBottomSheet(
+                      (context) => Stack(
+                        alignment: Alignment.bottomCenter,
+                        children: [
+                          GestureDetector(
+                            onTap: () {
+                              Navigator.pop(context);
+                            },
+                            child: Container(
+                              width: double.infinity,
+                              height: double.infinity,
+                              color: Color.fromRGBO(0, 0, 0, 0.5),
+                            )
+                          ),
+                          Container(
+                            width: double.infinity,
+                            height: 200,
+                            //color: Colors.transparent,
+                            color: Color.fromRGBO(25, 24, 29, 1),
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.center,
+                              children: [
+                                Row(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  children: [
+                                    Container(
+                                    margin: EdgeInsets.symmetric(vertical: 10),
+                                    child: Text(
+                                      "Você tem certeza que deseja sair da conta?",
+                                      style: TextStyle(color: Colors.white, fontSize: 20),
+                                    ),
+                                    )
+                                  ],
+                                ),
+                                Row(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  children: [
+                                    Container(
+                                    margin: EdgeInsets.symmetric(vertical: 10),
+                                    child: ElevatedButton(
+                                      onPressed: () {
+                                        ScaffoldMessenger.of(
+                                          context,
+                                        ).hideCurrentSnackBar();
+                                        Navigator.push(
+                                          context,
+                                          MaterialPageRoute(
+                                            builder: (context) => widget.destiny,
+                                          ),
+                                        );
+                                      },
+                                      style: ElevatedButton.styleFrom(
+                                        backgroundColor: Colors.red,
+                                        fixedSize: Size(
+                                          MediaQuery.of(context).size.width - 50,
+                                          50,
+                                        
+                                        ),
+                                        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(12))
+                                      ),
+                                      child: Text("Sair", style: TextStyle(color: Color.fromRGBO(25, 24, 29, 1),),),
+                                    ),
+                                    ),
+                                  ],
+                                ),
+                                Row(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  children: [
+                                    Container(
+                                    margin: EdgeInsets.symmetric(vertical: 10),
+                                    child: OutlinedButton(
+                                      onPressed: () {
+                                        Navigator.pop(context);
+                                      },
+                                      style: OutlinedButton.styleFrom(
+                                        fixedSize: Size(
+                                          MediaQuery.of(context).size.width - 50,
+                                          50,
+                                        ),
+                                        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(12))
+                                      ),
+                                      child: Text("Ficar", style: TextStyle(color: Colors.white),),
+                                    ),
+                                    ),
+                                  ],
+                                )
+                              ],
+                            ),
+                          ),
+                        ],
+                      ),
+                    backgroundColor: Colors.transparent);
+        },
         style: ElevatedButton.styleFrom(
           shadowColor: Colors.transparent,
           backgroundColor: Color.fromRGBO(25, 24, 29, 1),
