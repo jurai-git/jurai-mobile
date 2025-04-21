0a1213a91dd1d3a7111cdded7192b5fdec94f350
MateusDaroz <matlicoop@gmail.com>
2025-04-21 19:15:15 -0300

    add: requerentes topic information widget

---
+++ lib/features/home/view/widgets/requerentes_topic_information.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class RequerentesTopicInformation extends StatelessWidget {
+  final String topicName;
+  final String topicData;
+  final String topicImage;
+
+  const RequerentesTopicInformation({super.key, required this.topicName, required this.topicData, required this.topicImage});
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      spacing: 15,
+      children: [
+        Container(
+          padding: EdgeInsets.all(12),
+          decoration: BoxDecoration(
+            color: Color(0xFF387FB9),
+            borderRadius: BorderRadius.all(Radius.circular(5))
+          ),
+          child: Image.asset(topicImage, scale: .8,),
+        ),
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(topicName, style: TextStyle(color: Colors.white, fontSize: 12), textAlign: TextAlign.left,),
+            Text(topicData, style: TextStyle(color: Colors.white, fontSize: 18), textAlign: TextAlign.left,),
+          ]
+        )
+      ],
+    );
+  }
+}
