17c714eb5233d43b29eb7a92ec6464bb3a38bcf0
MateusDaroz <matlicoop@gmail.com>
2025-04-25 09:41:54 -0300

    add: conditional render based on current demanda existence and add single choice

--- lib/features/home/view/pages/documents.dart
+++ lib/features/home/view/pages/documents.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_svg/svg.dart';
+import 'package:jurai/features/auth/view/widgets/loading_circle.dart';
 import 'package:jurai/features/home/models/demanda.dart';
 import 'package:jurai/features/home/providers/demanda_provider.dart';
 import 'package:jurai/features/home/view/pages/profile.dart';
@@ -15,6 +16,9 @@ class Documents extends ConsumerStatefulWidget {
 
 var demandasList;
 var currentDemanda;
+int buttonIndex = 0;
+
+enum Options{ general, ement }
 
 class _DocumentsState extends ConsumerState<Documents>{ 
 
@@ -81,6 +85,90 @@ class _DocumentsState extends ConsumerState<Documents>{
                   ),
                 ],
               ),
+              currentDemanda == null ? Container(
+                padding: EdgeInsets.symmetric(horizontal: 50),
+                child: FutureBuilder<List<Widget>>(
+                  future: demandasList,
+                  builder: (context, snapshot) {
+                    if (snapshot.connectionState == ConnectionState.waiting) {
+                      return Padding(padding: EdgeInsets.only(top: MediaQuery.of(context).size.height/4-100), child: LoadingCircle());
+                    } else if (snapshot.hasError) {
+                      return Text('Error: ${snapshot.error}', style: TextStyle(color: Colors.white),);
+                    } else if (snapshot.hasData) {
+                      return Column(
+                        spacing: 15,
+                        children: snapshot.data!,
+                      );
+                    } else {
+                      return Text('Não foram encontradas demandas!', style: TextStyle(color: Colors.white),);
+                    }
+                  },
+                ),
+              )
+              :
+              Column(
+                spacing: 30,
+                children: [
+                  Container(
+                    decoration: BoxDecoration(
+                      border: Border(top: BorderSide(color: const Color.fromRGBO(255, 255, 255, .1)), bottom: BorderSide(color: const Color.fromRGBO(255, 255, 255, .1))),
+                    ),
+                    child: Padding(
+                      padding: EdgeInsets.symmetric(horizontal: 50, vertical: 15),
+                      child: Row(
+                        spacing: 15,
+                        children: [
+                          Container(
+                            decoration: BoxDecoration(
+                              color: Colors.blue,
+                              borderRadius: BorderRadius.all(Radius.circular(100)),
+                            ),
+                            padding: EdgeInsets.all(20),
+                            child: SvgPicture.asset("img/profile.svg"),
+                          ),
+                          Text("teste", style: TextStyle(color: Colors.white, fontSize: 20)),
+                        ]
+                      ),
+                    )
+                  ),
+                  Padding( 
+                    padding: EdgeInsets.symmetric(horizontal: 50),
+                    child: Column(
+                      spacing: 30,
+                      children: [
+                        Container(
+                          width: MediaQuery.of(context).size.width,
+                          child: SingleChoice(
+                            onSelectionChanged: (value) {
+                              setState(() {
+                                buttonIndex = value - 1;
+                              });
+                            },
+                          ),
+                        ),
+                        if (buttonIndex == 0) 
+                          Column(
+                            spacing: 30,
+                            children: [
+                            ],
+                          ),
+                        if (buttonIndex == 1) 
+                          Column(
+                            spacing: 30,
+                            children: [
+                            ],
+                          ),
+                        if (buttonIndex == 2)
+                          Column(
+                            spacing: 30,
+                            children: [
+                            ],
+                          ),
+                      ]
+                    ),
+                  )
+                ],
+              )
             ]
           )
         )
@@ -101,4 +189,35 @@ Future<List<Widget>> loadDemandas(BuildContext context, WidgetRef ref) async{
   print(lista);
   
   return lista;
+}
+
+class SingleChoice extends StatelessWidget {
+  final Function(int) onSelectionChanged;
+  const SingleChoice({super.key, required this.onSelectionChanged});
+
+  @override
+  Widget build(BuildContext context) {
+    return SegmentedButton<Options>(
+      showSelectedIcon: false,
+      style: SegmentedButton.styleFrom(
+        backgroundColor: Color.fromRGBO(25, 24, 29, 1),
+        foregroundColor: const Color.fromRGBO(255, 255, 255, .5),
+        selectedBackgroundColor: Colors.blue,
+        selectedForegroundColor: Colors.white,
+        side: BorderSide(color: Colors.transparent, width: 0),
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(5)),
+        textStyle: TextStyle(fontWeight: FontWeight.w400),
+      ),
+      segments: const <ButtonSegment<Options>>[
+        ButtonSegment<Options>(value: Options.general, label: Text('Gerais')),
+        ButtonSegment<Options>(value: Options.ement, label: Text('Ementa')),
+      ],
+      selected: <Options>{Options.values[buttonIndex]},
+      onSelectionChanged: (Set<Options> newSelection) {
+        Options selectedOption = newSelection.first;
+        int selectedValue = selectedOption == Options.general ? 1 : 2;
+        onSelectionChanged(selectedValue);
+      },
+    );
+  }
 }
