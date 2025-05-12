34a56e9b4985b9a83cc3c301a5a3ae3d389dbc6d
MateusDaroz <matlicoop@gmail.com>
2025-05-12 09:37:15 -0300

    add: getAllDemandasFromRequerente call when a requerente is selected

--- lib/features/home/view/widgets/requerentes_view_button.dart
+++ lib/features/home/view/widgets/requerentes_view_button.dart
@@ -3,7 +3,9 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:jurai/features/home/models/requerente.dart';
+import 'package:jurai/features/home/providers/demanda_provider.dart';
 import 'package:jurai/features/home/providers/requerente_provider.dart';
+import 'package:jurai/features/home/viewmodel/home_viewmodel.dart';
 
 class RequerentesViewButton extends StatefulWidget {
   final Requerente requerente;
@@ -22,6 +24,7 @@ class _RequerentesViewButtonState extends State<RequerentesViewButton> {
       onPressed: () {
         setState(() {
           widget.ref.watch(requerenteProvider.notifier).setRequerente(widget.requerente);
+          widget.ref.watch(homeViewModelProvider.notifier).getAllDemandasFromRequerente(id_requerente: widget.requerente.id_requerente);
         });
       },
       style: ElevatedButton.styleFrom(
