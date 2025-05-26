d54f466a5bbaa10b37af95bb35786fd207c80e9f
MateusDaroz <matlicoop@gmail.com>
2025-05-26 09:58:10 -0300

    add: recent access requerentes widget viewer

---
+++ lib/features/home/view/widgets/recent_access_requerente.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:jurai/features/home/models/requerente.dart';
+import 'package:jurai/features/home/providers/requerente_provider.dart';
+import 'package:jurai/features/home/view/pages/navigation.dart';
+
+class RecentAccessRequerente extends ConsumerStatefulWidget {
+  final Requerente requerente;
+
+  const RecentAccessRequerente({super.key, required this.requerente});
+
+  @override
+  ConsumerState<RecentAccessRequerente> createState() => _RecentAccessRequerenteState();
+}
+
+class _RecentAccessRequerenteState extends ConsumerState<RecentAccessRequerente> {
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+          onTap: (){
+            ref.read(requerenteProvider.notifier).setRequerente(widget.requerente);
+            Navigator.pushReplacement(context, MaterialPageRoute(builder: (_) => CustomBotNavBar(customIndex: 1)));
+          },
+          child: Column(
+            children: [
+              Container(
+              decoration: BoxDecoration(
+                color: Colors.blue,
+                borderRadius: BorderRadius.all(Radius.circular(100)),
+              ),
+              padding: EdgeInsets.all(65),
+              child: SvgPicture.asset("img/profile.svg"),
+            ),
+            Text(widget.requerente.nome.split(' ')[0] + " " +  widget.requerente.nome.split(' ')[1], style: TextStyle(color: Colors.white, fontSize: 28,)),
+            ],
+          ),
+    );
+  }
+}
