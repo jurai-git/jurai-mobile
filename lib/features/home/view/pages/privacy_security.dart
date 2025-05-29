686a6b67ef4178ef0481335ed446f2c85b51119a
MateusDaroz <matlicoop@gmail.com>
2025-05-29 17:27:24 -0300

    add: privacy and security page with widgets

--- lib/features/home/view/pages/privacy_security.dart
+++ lib/features/home/view/pages/privacy_security.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:jurai/features/home/view/widgets/profile_options_button.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class PrivacySecurity extends StatefulWidget {
   const PrivacySecurity({super.key});
@@ -10,6 +12,17 @@ class PrivacySecurity extends StatefulWidget {
 class _PrivacySecurityState extends State<PrivacySecurity> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(title: Text("Privacidade e Segurança", style: TextStyle(color: Colors.white)), centerTitle: true, iconTheme: IconThemeData(color: Color(0xFF387FB9)), backgroundColor: Colors.transparent,),
+      backgroundColor: Color.fromRGBO(25, 24, 29, 1),
+      body: Center(
+        child: Column(
+          children: [
+            ProfileOptionsButton(text: "Termos de Privacidade", preffixIcon: Icons.privacy_tip_outlined, destiny: Text("")),
+            ProfileOptionsButton(text: "Alteração de Senha", preffixIcon: Icons.lock_outline, destiny: Text(""), outsideUrl: true,),
+          ],
+        ),
+      ),
+    );
   }
 }
