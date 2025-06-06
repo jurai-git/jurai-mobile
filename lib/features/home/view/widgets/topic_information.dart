740af48bf16dd5c4ec5c2684bc26ae03d8e54e98
MateusDaroz <matlicoop@gmail.com>
2025-06-05 23:14:01 -0300

    refactor: topic information design

--- lib/features/home/view/widgets/topic_information.dart
+++ lib/features/home/view/widgets/topic_information.dart
@@ -6,30 +6,47 @@ class TopicInformation extends StatelessWidget {
   final String topicName;
   final String topicData;
   final String topicImage;
+  final bool first;
 
-  const TopicInformation({super.key, required this.topicName, required this.topicData, required this.topicImage});
+  const TopicInformation({
+    super.key,
+    required this.topicName,
+    required this.topicData,
+    required this.topicImage,
+    this.first=false
+  });
 
   @override
   Widget build(BuildContext context) {
-    return Row(
-      spacing: 15,
-      children: [
-        Container(
-          padding: EdgeInsets.all(12),
-          decoration: BoxDecoration(
-            color: Color(0xFF387FB9),
-            borderRadius: BorderRadius.all(Radius.circular(5))
+    return Container(
+      margin: first ? EdgeInsets.symmetric(vertical: 20) : EdgeInsets.only(bottom: 20),
+      child: TextFormField(
+        initialValue: topicData,
+        style: const TextStyle(color: Colors.white),
+        enabled: false,
+        decoration: InputDecoration(
+          prefixIcon: Container(
+            margin: EdgeInsets.fromLTRB(10, 0, 5, 0),
+            child: SvgPicture.asset(
+              topicImage,
+              width: 20,
+              height: 20,
+              fit: BoxFit.contain,
+              colorFilter: ColorFilter.mode(Color(0xFF387FB9), BlendMode.srcIn),
+            ),
           ),
-          child: SvgPicture.asset(topicImage, width: 30, height: 30,),
+          prefixIconConstraints: const BoxConstraints(
+            minWidth: 20,
+            minHeight: 20,
+          ),
+          disabledBorder: OutlineInputBorder(
+            borderSide: const BorderSide(color: Colors.grey, width: 1),
+            borderRadius: BorderRadius.circular(10),
+          ),
+          labelText: topicName,
+          labelStyle: const TextStyle(color: Colors.grey),
         ),
-        Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text(topicName, style: TextStyle(color: const Color.fromRGBO(255, 255, 255, .5), fontSize: 12), textAlign: TextAlign.left,),
-            Text(topicData, style: TextStyle(color: Colors.white, fontSize: 18), textAlign: TextAlign.left,),
-          ]
-        )
-      ],
+      ),
     );
   }
 }
