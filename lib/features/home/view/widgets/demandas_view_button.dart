5bee9aa727dd2ebc3c934ffd5bd86b9fbef3ce1e
MateusDaroz <matlicoop@gmail.com>
2025-06-07 23:00:05 -0300

    add: container outside of demandas button and verifications for button state (skeleton or true button)

--- lib/features/home/view/widgets/demandas_view_button.dart
+++ lib/features/home/view/widgets/demandas_view_button.dart
@@ -17,40 +17,44 @@ class DemandasViewButton extends StatefulWidget {
 class _DemandasViewButtonState extends State<DemandasViewButton> {
   @override
   Widget build(BuildContext context) {
-    return ElevatedButton(
-      onPressed: () {
-        setState(() {
-          Navigator.push(context, MaterialPageRoute(builder: (_) => DemandasDetails(demanda: widget.demanda)));
-        });
-      },
-      style: ElevatedButton.styleFrom(
-        backgroundColor: Color.fromRGBO(43, 41, 50, 50),
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.all(Radius.circular(15)),
-        ),
-        padding: EdgeInsets.symmetric(horizontal: 20, vertical: 20),
-        shadowColor: const Color.fromRGBO(255, 255, 255, 0.1),
-        elevation: 5,
-      ),
-      child: Row(
-        //mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        spacing: 20,
-        children: [
-          Container(
-            decoration: BoxDecoration(
-              color: Colors.blue,
-              borderRadius: BorderRadius.all(Radius.circular(100)),
+    return Container(
+      margin: EdgeInsets.only(bottom: 15),
+      child:
+        ElevatedButton(
+          onPressed: () {
+            setState(() {
+              Navigator.push(context, MaterialPageRoute(builder: (_) => DemandasDetails(demanda: widget.demanda)));
+            });
+          },
+          style: ElevatedButton.styleFrom(
+            backgroundColor: Color.fromRGBO(43, 41, 50, 50),
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.all(Radius.circular(15)),
             ),
-            padding: EdgeInsets.all(20),
-            child: Icon(Icons.description, color: Colors.white,),
+            padding: EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+            shadowColor: const Color.fromRGBO(255, 255, 255, 0.1),
+            elevation: 5,
           ),
-          Flexible(
-            child: FittedBox( 
-              child: Text(widget.demanda.identificacao, style: TextStyle(color: Colors.white, fontSize: 25), overflow: TextOverflow.ellipsis, maxLines: 1,),
-            )
-          )
-        ],
-      ),
+          child: Row(
+            //mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            spacing: 20,
+            children: [
+              Container(
+                decoration: BoxDecoration(
+                  color: Colors.blue,
+                  borderRadius: BorderRadius.all(Radius.circular(100)),
+                ),
+                padding: widget.demanda.id != -1 ? EdgeInsets.all(20) : EdgeInsets.all(30),
+                child: widget.demanda.id != -1 ? Icon(Icons.description, color: Colors.white,) : null,
+              ),
+              Flexible(
+                child: FittedBox( 
+                  child: Text(widget.demanda.id != -1 ? widget.demanda.identificacao : "placeholder", style: TextStyle(color: Colors.white, fontSize: 25), overflow: TextOverflow.ellipsis, maxLines: 1,),
+                )
+              )
+            ],
+          ),
+      )
     );
   }
 }
