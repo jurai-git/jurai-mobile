d639ff883d315c590fbd2bd1ca06ce2488e5e308
MateusDaroz <matlicoop@gmail.com>
2025-06-07 22:43:45 -0300

    add: container outside of button and verifications for button state(skeleton or true button)

--- lib/features/home/view/widgets/requerentes_view_button.dart
+++ lib/features/home/view/widgets/requerentes_view_button.dart
@@ -21,42 +21,46 @@ class RequerentesViewButton extends StatefulWidget {
 class _RequerentesViewButtonState extends State<RequerentesViewButton> {
   @override
   Widget build(BuildContext context) {
-    return ElevatedButton(
-      onPressed: () {
-        setState(() {
-          Navigator.push(context, MaterialPageRoute(builder: (_) => ClientsInformation(requerente: widget.requerente)));
-          widget.ref.watch(recentReqAcessListProvider.notifier).setRecentReqAcessList(widget.requerente);
-          widget.ref.watch(homeViewModelProvider.notifier).getAllDemandasFromRequerente(id_requerente: widget.requerente.id_requerente);
-        });
-      },
-      style: ElevatedButton.styleFrom(
-        backgroundColor: Color.fromRGBO(43, 41, 50, 50),
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.all(Radius.circular(15)),
-        ),
-        padding: EdgeInsets.symmetric(horizontal: 20, vertical: 20),
-        shadowColor: const Color.fromRGBO(255, 255, 255, 0.1),
-        elevation: 5,
-      ),
-      child: Row(
-        //mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        spacing: 20,
-        children: [
-          Container(
-            decoration: BoxDecoration(
-              color: Colors.blue,
-              borderRadius: BorderRadius.all(Radius.circular(100)),
+    return Container(
+      margin: EdgeInsets.only(bottom: 15),
+      child:
+        ElevatedButton(
+          onPressed: () {
+            setState(() {
+              Navigator.push(context, MaterialPageRoute(builder: (_) => ClientsInformation(requerente: widget.requerente)));
+              widget.ref.watch(recentReqAcessListProvider.notifier).setRecentReqAcessList(widget.requerente);
+              widget.ref.watch(homeViewModelProvider.notifier).getAllDemandasFromRequerente(id_requerente: widget.requerente.id_requerente);
+            });
+          },
+          style: ElevatedButton.styleFrom(
+            backgroundColor:Color.fromRGBO(43, 41, 50, 50),
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.all(Radius.circular(15)),
             ),
-            padding: EdgeInsets.all(20),
-            child: SvgPicture.asset("img/profile.svg"),
+            padding: EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+            shadowColor: const Color.fromRGBO(255, 255, 255, 0.1),
+            elevation: 5,
           ),
-          Flexible(
-            child: FittedBox( 
-              child:Text(widget.requerente.nome, style: TextStyle(color: Colors.white, fontSize: 25,), overflow: TextOverflow.ellipsis, maxLines: 1,),
-            )
-          )
-        ],
-      ),
+          child: Row(
+            //mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            spacing: 20,
+            children: [
+              Container(
+                decoration: BoxDecoration(
+                  color: Colors.blue,
+                  borderRadius: BorderRadius.all(Radius.circular(100)),
+                ),
+                padding: widget.requerente.id_requerente != -1 ? EdgeInsets.all(20) : EdgeInsets.all(30),
+                child: widget.requerente.id_requerente != -1 ? SvgPicture.asset("img/profile.svg") : null,
+              ),
+              Flexible(
+                child: FittedBox( 
+                  child:Text(widget.requerente.nome == '' ? "placeholder" : widget.requerente.nome, style: TextStyle(color: Colors.white, fontSize: 25,), overflow: TextOverflow.ellipsis, maxLines: 1,),
+                )
+              )
+            ],
+          ),
+        ),
     );
   }
 }
