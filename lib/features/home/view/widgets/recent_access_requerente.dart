f4093590d7a7275592a77be1d9024a1a35737027
MateusDaroz <matlicoop@gmail.com>
2025-08-10 23:46:39 -0300

    refactor: style of recent access requerente widget

--- lib/features/home/view/widgets/recent_access_requerente.dart
+++ lib/features/home/view/widgets/recent_access_requerente.dart
@@ -19,35 +19,30 @@ class RecentAccessRequerente extends ConsumerStatefulWidget {
 class _RecentAccessRequerenteState extends ConsumerState<RecentAccessRequerente> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-        margin: EdgeInsets.only(right: 20),
-        padding: EdgeInsets.all(10),
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.all(Radius.circular(12)),
-          color: Color.fromARGB(255, 32, 31, 36),
-        ),
+    return GestureDetector(
+      onTap: () {
+        ref.read(requerenteProvider.notifier).setRequerente(widget.requerente);
+        ref.watch(homeViewModelProvider.notifier).getAllDemandasFromRequerente(id_requerente: widget.requerente.id_requerente);
+        Navigator.push(context, MaterialPageRoute(builder: (_) => ClientsInformation(requerente: widget.requerente)));
+      },
+      child: Container(
+        margin: EdgeInsets.only(right: 15),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             Container(
               decoration: BoxDecoration(
-                color: Colors.blue,
-                borderRadius: BorderRadius.all(Radius.circular(100)),
+                  color: Color.fromRGBO(20, 40, 60, 0.25),
+                  borderRadius: BorderRadius.all(Radius.circular(15)),
               ),
-              padding: EdgeInsets.all(50),
-              child: SvgPicture.asset("img/profile.svg"),
+              padding: EdgeInsets.all(20),
+              child: Text(widget.requerente.nome.trim().split(' ').map((l) => l[0]).take(2).join(), style: TextStyle(color: Color(0xFF387FB9), fontSize: 24, fontWeight: FontWeight.bold, letterSpacing: 1.5),),
             ),
-            Text(widget.requerente.nome.split(' ')[0] + " " +  widget.requerente.nome.split(' ')[1], style: TextStyle(color: Colors.white, fontSize: 28,)),
-            TextButton(
-              onPressed: (){
-                ref.read(requerenteProvider.notifier).setRequerente(widget.requerente);
-                ref.watch(homeViewModelProvider.notifier).getAllDemandasFromRequerente(id_requerente: widget.requerente.id_requerente);
-                Navigator.push(context, MaterialPageRoute(builder: (_) => ClientsInformation(requerente: widget.requerente)));
-              }, 
-              child: Text("Ver detalhes", style: TextStyle(color: Color(0xFF387FB9),),)
-            )
+            Text(widget.requerente.nome.split(' ')[0], style: TextStyle(color: Colors.white,)),
+            Text(widget.requerente.nome.split(' ')[1], style: TextStyle(color: Colors.white,)),
           ],
-        ),
+        )
+      )
     );
   }
 }
